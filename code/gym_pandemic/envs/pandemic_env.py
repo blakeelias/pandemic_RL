7d1375e51214035663d79f4732c0450fc7380b77
Blake Elias <blakeelias@gmail.com>
2020-09-06 02:57:00 +0000

    Get gym environment to run

--- code/gym_pandemic/envs/pandemic_env.py
+++ code/gym_pandemic/envs/pandemic_env.py
@@ -5,7 +5,7 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 
 
-class PandemicEnvironment(gym.Env):
+class PandemicEnv(gym.Env):
   """Custom Environment that follows gym interface"""
   metadata = {'render.modes': ['human']}
 
@@ -16,7 +16,7 @@ class PandemicEnvironment(gym.Env):
                imported_cases_per_step=0.5,
                power=2,
                scale_factor=100):
-    super(CustomEnv, self).__init__()
+    super(PandemicEnv, self).__init__()
     self.num_population = num_population
     self.initial_num_cases = initial_num_cases
     self.R_0 = R_0
@@ -33,13 +33,13 @@ class PandemicEnvironment(gym.Env):
     
     # assumes num_population = 1000
     self.states_num_infected = sum([list(range(100*i, 100*(i+1), i+1)) for i in range(10)], []) + [1_000]
-    self.lookup = dict([(x, i) for i in range(num_states - 1) for x in range(states[i], states[i+1])] + [(num_population, len(states) - 1)])
+    num_states = len(self.states_num_infected)
+    self.lookup = dict([(x, i) for i in range(num_states - 1) for x in range(self.states_num_infected[i], self.states_num_infected[i+1])] + [(num_population, num_states - 1)])
 
     # Use entire state space
     #self.observation_space = spaces.Box(low=0,
     #                                    high=num_population,
     #                                    shape=(1,), dtype=np.uint16)  # maximum infected = 2**16 == 65536
-    num_states = len(self.states_num_infected)
     self.observation_space = spaces.Discrete(num_states)
 
     self.state = [self._bucket_state_index(self.initial_num_cases)]
@@ -72,13 +72,13 @@ class PandemicEnvironment(gym.Env):
     # Reset the state of the environment to an initial state
     self.state = [self._bucket_state_index(self.initial_num_cases)]
     
-    return self._next_observation()
+    # return self._next_observation()
     
   def render(self, mode='human', close=False):
     # Render the environment to the screen
     print(self.state)
 
-  def _reward(num_infected, r, power=power, **kwargs):
+  def _reward(self, num_infected, r, **kwargs):
     return -self._cost_of_n(num_infected, **kwargs) - self._cost_of_r(r, **kwargs)
     
   def _cost_of_r(self, r, **kwargs):
@@ -98,31 +98,28 @@ class PandemicEnvironment(gym.Env):
     else:
       return n
 
-  def _expected_new_state(self, current_state, action, imported_cases_per_step=imported_cases_per_step, **kwargs):
-    r_effective = action
-    current_cases = current_state
-    
+  def _expected_new_state(self, num_cases, r, **kwargs):    
     fraction_susceptible = 1 # (num_population - current_cases) / num_population
     # TODO: may need better way to bound susceptible population,
     # to account for immunity
     # One option: fraction_susceptible = 1 always, and just bound new_state by num_population
 
     # Better solution: keep track of how many people are susceptible NOW, based on some immunity time
-    expected_new_cases = (current_cases * r_effective) * fraction_susceptible + imported_cases_per_step
+    expected_new_cases = (num_cases * r) * fraction_susceptible + self.imported_cases_per_step
 
     return expected_new_cases
   
   def _bucket_state_index(self, new_num_cases):
     if new_num_cases < 0:
         raise Exception('Cannot have negative case count')
-    elif new_num_cases in lookup:
+    elif new_num_cases in self.lookup:
         return self.lookup[new_num_cases]
     else:
         return self.lookup[self.num_population]
 
   def _new_state_distribution(self, state, action, distr_family='poisson', **kwargs):
     # distr_family: 'poisson' or 'nbinom'
-    lam = self.expected_new_state(state, action, **kwargs)
+    lam = self._expected_new_state(state, action, **kwargs)
     
     if distr_family == 'poisson':
         return poisson(lam)
