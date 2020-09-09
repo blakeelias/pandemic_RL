3a98a426f2f0b90a857045fa1f3c800ec2c08786
Blake Elias <blakeelias@gmail.com>
2020-09-09 23:01:16 +0000

    Better plotting code. Bug in capping number of cases above 1000?

--- code/main.py
+++ code/main.py
@@ -1,3 +1,6 @@
+from itertools import product
+from collections import namedtuple
+
 import gym
 from tqdm import tqdm
 import argparse
@@ -7,50 +10,69 @@ from test import test_environment
 from gym_pandemic.envs.pandemic_env import PandemicEnv
 
 
+Params = namedtuple('Params', ['imported_cases_per_step', 'power', 'extra_scale'])
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Boolean command-line')
 
-    parser.add_argument('--imported_cases_per_step', metavar='imported_cases_per_step', type=float, nargs='+', default=[0.0, 0.5, 1.0, 5.0, 10.0], help='')
+    parser.add_argument('--imported_cases_per_step',
+                        metavar='imported_cases_per_step',
+                        type=float,
+                        nargs='+',
+                        default=[0.0, 0.5, 1.0, 5.0, 10.0], help='')
 
-    parser.add_argument('--powers', metavar='powers', type=float, nargs='+', help='')
+    parser.add_argument('--powers',
+                        metavar='powers',
+                        type=float,
+                        nargs='+',
+                        help='')
 
-    parser.add_argument('--extra_scale', metavar='extra_scale', type=float, nargs='+', help='')
+    parser.add_argument('--extra_scale',
+                        metavar='extra_scale',
+                        type=float,
+                        nargs='+',
+                        help='')
 
     return parser.parse_args()
+
+
+power_scale_factor = {
+    .1: 2000,
+    .25: 700,
+    .5: 275,
+    1.0: 70,
+    1.5: 22,
+}
+
 
 def main(imported_cases_per_step_range=[0.0, 0.5, 1.0, 5.0, 10.0],
          powers=[0.1, 0.25, 0.5, 1.0, 1.5],
          extra_scale=[0.25, 1.0]):
-    # imported_cases_per_step_range = [0, 0.5, 1.0, 5.0, 10.0])
-    # power_range = np.arange(1.0, 3.0, 0.5)
-    # scale_factor_range = [100, 500, 1000]
 
-    # power_scale_factor_range = [0.25, 1]
+    params_sweep = [Params(*params)
+                    for params in product(imported_cases_per_step_range, powers, extra_scale)]
 
-    power_scale_factor = {
-        .1: 2000,
-        .25: 700,
-        .5: 275,
-        1.0: 70,
-        1.5: 22
-    }
-
-    print(imported_cases_per_step_range)
-    print(powers)
-    print(extra_scale)
+    policies = {}
+    Vs = {}
     
-    for imported_cases_per_step in tqdm(imported_cases_per_step_range):
-        for power in tqdm(powers):
-            for extra in tqdm(extra_scale):
-                env = PandemicEnv(imported_cases_per_step=imported_cases_per_step,
-                                  power=power,
-                                  scale_factor=power_scale_factor[power]*extra)
-                policy, V = train_environment(env)
-                test_environment(env, policy)
-                
-    #    for power in tqdm(power_range):
-    #        for scale_factor in tqdm(scale_factor_range):
+    for params in tqdm(params_sweep):
+        env = make_environment(params)
+        policy, V = train_environment(env)
+        policies[params] = policy
+        Vs[params] = V
 
+    for params in params_sweep:
+        env = make_environment(params)
+        print('params', params)
+        test_environment(env, policies[params], Vs[params])
+        
+    
+def make_environment(params: 'Params'):
+    return PandemicEnv(imported_cases_per_step=params.imported_cases_per_step,
+                       power=params.power,
+                       scale_factor=power_scale_factor[params.power] * params.extra_scale)
+    
 
 if __name__ == '__main__':
     args = parse_args()
--- code/test.py
+++ code/test.py
@@ -1,21 +1,33 @@
 from matplotlib import pyplot as plt
 
 
-def test_environment(env, policy):
+def test_environment(env, policy, V=None):
     env.reset()
     gamma = 0.99
 
     num_infected = []
-    actions_r = []
+    actions_taken = []
+
+    actions_policy = [env.actions_r[policy[i].argmax()] for i in range(env.nS)]
+
+    print('policy')
+    plt.plot(range(env.nS), actions_policy)
+    plt.show()
+
+    print('value function')
+    plt.plot(range(env.nS), V)
+    plt.show()
     
-    print('R \t total reward')
     observation = env.reset()
     total_reward = 0
     gamma_cum = 1
+
+    print(f'initial num infected: {env.state[0]}')
+    
     for t in range(100):
         # Get best action
         action = policy[observation].argmax()
-        actions_r.append(env.actions_r[action])
+        actions_taken.append(env.actions_r[action])
         observation, reward, done, info = env.step(action)
         num_infected.append(observation)
         
@@ -24,9 +36,17 @@ def test_environment(env, policy):
             break
         total_reward += gamma_cum * reward
         gamma_cum *= gamma
-    print(f'{env.actions_r[action]} \t {total_reward}')
+
+    
+    
+    print('num infected')
     plt.plot(range(len(num_infected)), num_infected)
     plt.show()
-    plt.plot(range(len(actions_r)), actions_r)
+
+    print('action taken')
+    plt.plot(range(len(actions_taken)), actions_taken)
     plt.show()
+
+    print(f'total reward: {total_reward}')
+    
     env.close()
