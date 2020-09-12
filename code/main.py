3547fb88dc31905a7c338166c3b921b250cf8959
Blake Elias <blakeelias@gmail.com>
2020-09-12 01:42:06 +0000

    Fix args passing; start experiment

--- code/main.py
+++ code/main.py
@@ -49,9 +49,11 @@ power_scale_factor = {
 }
 
 
-def main(imported_cases_per_step_range=[0.0, 0.5, 1.0, 5.0, 10.0],
-        powers=[0.1, 0.25, 0.5, 1.0, 1.5],
-        extra_scale=[0.25, 1.0]):
+def main(args={
+          'imported_cases_per_step_range': [0.0, 0.5, 1.0, 5.0, 10.0],
+          'powers': [0.1, 0.25, 0.5, 1.0, 1.5],
+          'extra_scale': [0.25, 1.0]
+         }):
 
     experiment_parameters = {
         'distr_family': 'poisson',
