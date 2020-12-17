30a72a8eeeb335d20157c257d03fa3df5153fbed
Blake Elias <blakeelias@gmail.com>
2020-12-17 17:51:07 +0000

    Better README run instructions; better file name for results

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ cd code; pip install -e .
 
 ## Run
 
+"Hello world":
+```
+python main.py --imported_cases_per_step_range 0 --num_population 100
+```
+
+Full experiment:
+```
+python main.py --num_population 1000 --power_scale_factors 0.25 1.0 --imported_cases_per_step_range 0 0.5 --powers 0.1 0.25 0.5 1.0 1.5
+
+python main.py --num_population 1000 --power_scale_factors 0.25 1.0 --imported_cases_per_step_range 1.0 5.0 10.0 --powers 0.1 0.25 0.5 1.0 1.5
+```
--- code/train.py
+++ code/train.py
@@ -12,7 +12,7 @@ DISCOUNT_FACTOR=0.99
 
 def train_environment(env, theta=0.0001, discount_factor=DISCOUNT_FACTOR):
     reward_param_str = env.reward_param_str + f',theta={theta},discount_factor={discount_factor}'
-    file_name_prefix = f'../results/{env.dynamics_param_str}/policy_reward=({reward_param_str}),env=({env.dynamics_param_str})'
+    file_name_prefix = f'../results/env=({env.dynamics_param_str})/reward=({reward_param_str})'
     file_name = file_name_prefix + '.pickle'
 
     try:
@@ -37,7 +37,3 @@ if __name__ == '__main__':
     policy, V = train_environment(env)
     test_environment(env, policy)
     
-
-# python main.py --power_scale_factors 0.25 1.0 --imported_cases_per_step_range 0 0.5 --powers 0.1 0.25 0.5 1.0 1.5
-
-# python main.py --power_scale_factors 0.25 1.0 --imported_cases_per_step_range 1.0 5.0 10.0 --powers 0.1 0.25 0.5 1.0 1.5
