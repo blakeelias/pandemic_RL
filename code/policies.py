fe7f3d8f4111a5efd793dbf6eba3fb98bc1fda0c
Blake Elias <blakeelias@gmail.com>
2021-01-19 21:25:22 +0000

    Policy for keeping R <= 1

---
+++ code/policies.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def policy_fn_generator(policy):
+    def policy_fn(env, state_idx, time_idx):
+        return policy[state_idx].argmax()
+    return policy_fn
+
+
+def policy_fn_R_eq_1(env, state_idx, time_idx):
+    state = env.states[state_idx]
+    num_susceptible, num_infected = state
+
+    # TODO: allow continuous action space?
+    R_ts = np.array([env.R_t(action, time_idx) for action in range(env.nA)])
+    valid_actions = np.where(R_ts <= 1)[0]
+    valid_Rts = R_ts[valid_idxs]
+    action = valid_actions[valid_Rts.argmax()]
+
+    print(f'Action index chosen: {action}')
+    print(f'R_t: {R_ts[action]}')
+    
+    return action
